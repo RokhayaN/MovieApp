ff8f0951e057c6decf23b4832580c6ddc9f5249c
github username <github email address>
2020-03-11 17:40:24 -0500

    Done.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,10 @@
 class MoviesController < ApplicationController
     def index 
-    @movies = Movie.all
+       @movies = Movie.all
     end 
     
     def show 
-    @movie = Movie.find(params[:id])
-    @actors = @movie.actors
+       @movie = Movie.find(params[:id])
+       @actors = @movie.actors
     end
 end
