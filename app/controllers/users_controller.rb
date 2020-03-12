bc694ee8bd592815cd964fa01aa0571bc4faa6ee
github username <github email address>
2020-03-11 20:10:31 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end 
-
+private 
+    def user_params 
+        params.require(:user).permit.(:first_name, :last_name, :email, :password)
+    end 
 end
