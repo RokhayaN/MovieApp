dea0ec61e4431c79fd2fceb97c224e02580ce2aa
github username <github email address>
2020-03-12 11:47:28 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,14 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save 
             session[:user_id]= @user.id 
-            redirect_to '/'
+            redirect_to movies_path
         else
-            redirect_to '/signup' 
+        render :new 
         end 
     end 
 
 private 
     def user_params 
-        params.require(:user).permit.(:first_name, :last_name, :email, :password)
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
     end 
 end
