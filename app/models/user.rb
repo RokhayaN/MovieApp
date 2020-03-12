5a8730435b03612b362e75da875e659ec1cbffc3
github username <github email address>
2020-03-12 13:09:37 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-    has_secure_password
+    has_many :movies
+  validates_presence_of :first_name, :last_name, :password
+  validates_uniqueness_of :email
+  has_secure_password
 end
