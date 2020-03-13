5e1360bf9e7c9284ef42de68068bb06a3506ebac
github username <github email address>
2020-03-13 13:43:19 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_223628) do
+ActiveRecord::Schema.define(version: 2020_03_13_153721) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -41,9 +41,7 @@ ActiveRecord::Schema.define(version: 2020_03_11_223628) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
+    t.string "username"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
