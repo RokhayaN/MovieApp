48cdc971aa9879dba8206b5ce6305f79f3619c2c
github username <github email address>
2020-03-15 17:58:10 -0500

    Done.

---
+++ db/migrate/20200315225222_add_email_on_users.rb
@@ -0,0 +1,4 @@
+class AddEmailOnUsers < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_170013) do
+ActiveRecord::Schema.define(version: 2020_03_15_225222) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
