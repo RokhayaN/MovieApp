15d4211dc23b49e406cc4bdfe45878b9f93ecb1f
github username <github email address>
2020-03-16 16:10:51 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,17 @@
 class ApplicationController < ActionController::Base 
+  helper_method :current_user 
   
+
     def current_user
         @user = User.find_by(id: session[:user_id])
       end
     
-      def require_login
-        redirect_to movies_path unless logged_in?
-      end   
-    
-      def logged_in?
-        !!current_user
-      end
-    
-      def log_in(user)
-        session[user_id] = user.id
-      end 
+      def require_user
+        redirect_to '/login' unless current_user
+       end
+       
+       def require_author
+        redirect_to '/' unless current_user.author?
+       end
 end
 
